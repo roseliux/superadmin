03c136d6c4b4e39d62b97537ee09b38b9ca3d13a
Rocela Durazo <roxdurazo@gmail.com>
2019-11-30 15:56:47 -0700

    Add rubocop and fix offenses

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,9 +1,10 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get root_url
     assert_response :success
   end
-
 end
