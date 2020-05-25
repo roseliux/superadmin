0ea6b066fd058668d39dc42d34b80c720fcf213d
Rocela Durazo <roxdurazo@gmail.com>
2020-05-24 17:56:49 -0700

    Update dockerfile and add postgresql as the main db. (#16)
    * Update dockerfile and add postgresql as the main db.
    
    * Fix rubocop offenses.
    
    * Add service postgres to github action.
    
    * Fix bundle audit by updating rails
    
    * Change host to localhost to see if fix the build.
    
    * Move pg host into env variable.
    
    * Renamed Tab to IndentationStyle to fix obsolete configuration in rubocop.
    
    * Update todo

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_12_26_170051) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
